0ac5412c2115e7b70a72e426f5b8d96452cdd30e
liaohui <214271802@qq.com>
2018-03-27 23:48:48 +0800

    httpserver服务器响应

--- app/src/main/java/cn/lh/learnproject/httpserver/HttpServerTest.java
+++ app/src/main/java/cn/lh/learnproject/httpserver/HttpServerTest.java
@@ -1,10 +1,14 @@
 package cn.lh.learnproject.httpserver;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.cert.CRL;
+import java.util.Date;
 
 /**
  * Created by liaohui on 2018/3/26.
@@ -31,6 +35,7 @@ public class HttpServerTest {
     public void receive() {
         try {
             Socket socket = serverSocket.accept();
+            //客户端请求信息
             String msg = null;
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             StringBuilder builder = new StringBuilder();
@@ -42,6 +47,33 @@ public class HttpServerTest {
                 }
             }
             System.out.println(builder.toString());
+
+            //服务器响应
+
+            StringBuilder responseContext = new StringBuilder();
+            responseContext.append("你好啊");
+
+
+            StringBuilder response = new StringBuilder();
+            //1.HTTP协议版本、状态代码、描述
+            response.append("HTTP/1.1").append(BLANK).append("200").append(BLANK).append("OK").append(CRLF);
+            //2.响应头
+            response.append("Server:liaohui Server/0.0.1").append(CRLF)
+                    .append("Date:").append(new Date()).append(CRLF);
+            //相应的正文类型
+            response.append("Content-type:text/html;charset=utf8").append(CRLF);
+            //正文长度
+            response.append("Content-Length").append(responseContext.toString().getBytes().length).append(CRLF);
+            //3.正文之前
+            response.append(CRLF);
+            //4.正文
+            response.append(responseContext);
+
+            //输出流
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedWriter.write(response.toString());
+            bufferedWriter.flush();
+            bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -51,5 +83,7 @@ public class HttpServerTest {
 
     }
 
+    public static final String CRLF = "\r\n";
+    public static final String BLANK = " ";
 
 }
