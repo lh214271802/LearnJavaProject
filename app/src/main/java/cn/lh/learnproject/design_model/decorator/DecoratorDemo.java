919eecad3a1b17374e4d4f5ce04c35148e56a3f7
liaohui <214271802@qq.com>
2018-04-03 23:20:27 +0800

    装饰模式

--- app/src/main/java/cn/lh/learnproject/design_model/decorator/DecoratorDemo.java
+++ app/src/main/java/cn/lh/learnproject/design_model/decorator/DecoratorDemo.java
@@ -2,6 +2,7 @@ package cn.lh.learnproject.design_model.decorator;
 
 /**
  * 装饰模式
+ * BufferedInputStream以及BufferedOutputStream就是典型的装饰者模式
  */
 public class DecoratorDemo {
     public static void main(String[] args) {
